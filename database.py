815eec98911a0aa4855ac0db78682fab6fa01682
kacem <kacemmeherzi52@gmail.com>
2024-12-05 15:00:33 +0100

    endpoint added

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://kacem:kacemca1920@endpoint/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://kacem:kacemca1920@database-2.cizzk9mkg1wx.us-east-1.rds.amazonaws.com/postgres"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
